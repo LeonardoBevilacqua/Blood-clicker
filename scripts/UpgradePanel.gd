4b4670c0dba42582d9f8b15af2e62a98db875d8b
LeonardoBevilacqua <leonardo_bevilacqua@hotmail.com>
2019-09-13 20:57:56 -0300

    carregando recurso pelo json

--- scripts/UpgradePanel.gd
+++ scripts/UpgradePanel.gd
@@ -5,14 +5,10 @@ signal update_counter_per_second
 var upgradeButton = preload("res://scenes/components/UpgradeButton.tscn")
 var _counter = 0
 
+var buttons = []
+
 func _ready():
-	for i in range(5):
-		var bt = upgradeButton.instance()
-		
-		bt.init(str("teste ", i+1), i+1, i+1)
-		bt.connect("button_click", self, "_button_click")
-		
-		$Control/MarginContainer/VBoxContainer.add_child(bt)
+	self._generate_buttons(self._load_upgrades())
 
 func _button_click(button):
 	if (int(_counter) < button._price):
@@ -23,3 +19,46 @@ func _button_click(button):
 	
 func set_counter(counter):
 	self._counter = counter
+	self._set_buttons_status()
+	
+func _load_upgrades():
+	var file = File.new()
+	file.open("res://resources/upgrades.json", file.READ)
+	
+	var json = parse_json(file.get_as_text())
+	file.close()
+	
+	return json.upgrades
+	
+func _generate_buttons(upgrades):
+	for upgrade in upgrades:
+		var button = upgradeButton.instance()
+		
+		button.init(upgrade.description, upgrade.value, upgrade.price)
+		button.connect("button_click", self, "_button_click")
+		
+		if upgrade.price > _counter:
+			button.get_node("HBoxContainer/Button").disabled = true
+			button.get_node("HBoxContainer/Rows").hide()
+		
+		buttons.append(button)
+		
+		$Control/MarginContainer/VBoxContainer.add_child(button)
+
+func _set_buttons_status():
+	for button in buttons:
+		if !is_instance_valid(button):
+			buttons.erase(button)
+			continue
+		
+		if _counter >= button._price:
+			button.get_node("HBoxContainer/Button").disabled = false
+			button.get_node("HBoxContainer/Rows").show()
+		elif _counter < button._price:
+			button.get_node("HBoxContainer/Button").disabled = true
+
+
+
+
+
+
